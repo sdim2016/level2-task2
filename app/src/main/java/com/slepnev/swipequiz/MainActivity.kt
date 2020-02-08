4d955a4e0ed687c257dfde7e431ad6e3b9eb2219
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-08 14:52:13 +0100

    Added some questions from Merriam-Webster's True or False

--- app/src/main/java/com/slepnev/swipequiz/MainActivity.kt
+++ app/src/main/java/com/slepnev/swipequiz/MainActivity.kt
@@ -12,8 +12,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val questions = arrayListOf<Question>(
-        Question("This statement is false", false),
-        Question(("This statement is true"), true)
+        Question("Ostriches bury their heads up to 18 inches in the sand.", false),
+        Question("Louis Braille, creator of the Braille system of writing for the blind, was himself blind.", true),
+        Question("According to the Declaration of Independence, the people \"have nothing to lose but their chains.\"", false),
+        Question("The Oscar-winning film Guess Who's Coming to Dinner (1967) deals with interracial marriage.", true),
+        Question("Flight recorders onboard planes are painted black boxes.",false),
+        Question("The \"China Syndrome\" is what hostages experience when they begin to feel empathy for their captors.",false),
+        Question("A penny dropped from a tall building can reach sufficient velocity to kill a pedestrian below.",false),
+        Question("Harry Potter's archenemy is Tom Marvolo Riddle.",true),
+        Question("Other than water, coffee is the world's most popular drink.", true)
     )
     private val questionAdapter = QuestionAdapter(questions)
 
