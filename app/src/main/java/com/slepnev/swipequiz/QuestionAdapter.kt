cb56acef53481a770e5bbbbb441723648f31a86a
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-08 13:53:31 +0100

    Implemented the required adapter methods

--- app/src/main/java/com/slepnev/swipequiz/QuestionAdapter.kt
+++ app/src/main/java/com/slepnev/swipequiz/QuestionAdapter.kt
@@ -1,10 +1,26 @@
 package com.slepnev.swipequiz
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_question.view.*
 
-class QuestionAdapter {
+class QuestionAdapter(private val questions:List<Question>) : RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_question, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return questions.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(questions[position])
+    }
+
     inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
         fun bind(question: Question) {
             itemView.tvQuestion.text = question.questionText
